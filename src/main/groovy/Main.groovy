1aa341a0432fdc0775018999854cc7ef08ad56d7
GuillemVaquer <vaker2@gmail.com>
2025-03-04 11:27:23 +0100

    Segundo intento de refactorizar.

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -3,6 +3,9 @@ public class Main {
   static void main(String[] args) {
     println "Hello world!"
     println "Primera refactorización."
+    println "Cambio hecho para hacer un pull"
+    println "Ahora merge"
+    println "primera refactorizacion, segundo intento"
   }
 
 }
